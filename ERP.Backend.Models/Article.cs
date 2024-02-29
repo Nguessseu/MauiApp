0ec8c4742ce4ff08ced41eae3e7cb00c41725159
Nguesseu <armand.njomo@prodato.de>
2024-02-29 13:03:20 +0100

    first code

---
+++ ERP.Backend.Models/Article.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Models
+{
+    [DataContract]
+    public class Article
+    {
+        [DataMember(Order = 1)]
+        [Key]        
+        public int Id { get; set; }
+
+        [DataMember(Order = 2)]
+        public string Name { get; set; } = string.Empty;
+
+        [DataMember(Order = 3)]
+        public string Brand { get; set; } = string.Empty;
+
+        [DataMember(Order = 4)]
+        public List<Price> Prices { get; set; } = [];
+    }
+}
---
+++ ERP.Backend.Models/Price.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ERP.Backend.Models
+{
+    [DataContract]
+    public class Price
+    {
+        [DataMember]
+        [Key]        
+        public int Id { get; set; }
+        [DataMember]
+        [ForeignKey(nameof(Article))]
+        public int ArticleId { get; set; }
+        [DataMember]
+        public DateTime ValidFrom { get; set; }
+        [DataMember]
+        public decimal Amount { get; set; }
+        [IgnoreDataMember]
+        [JsonIgnore]
+        public Article? Article { get; set; }
+    }
+}
